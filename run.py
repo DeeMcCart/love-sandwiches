4699d5578c2b862c3fe586890e1584ce17518036
Deirdre McCarthy <deirdremc1968@gmail.com>
2023-06-20 12:41:40 +0000

    Collect sales data for last 5 markets

--- run.py
+++ run.py
@@ -77,6 +77,20 @@ def update_worksheet(data, worksheet):
     worksheet_to_update.append_row(data)
     print(f"{worksheet} worksheet updated successfully.\n")
 
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from saes worksheet, 
+    collecting the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+    columns = []
+    for ind in range(1, 7):
+        print(ind)
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    return columns
+
 
 def main():
     """
@@ -90,4 +104,5 @@ def main():
     update_worksheet(new_surplus_data, "surplus")
 
 print("Welcome to love sandwiches Data Automation")
-main()
+# main()
+sales_columns = get_last_5_entries_sales()
